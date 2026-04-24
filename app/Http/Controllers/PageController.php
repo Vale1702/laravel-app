053adc7e9dd24a05fc872155c4fc25187a1f1dcb
Valentina <valentinahdz0296@gmail.com>
2026-04-23 18:37:32 -0600

    Creacion de controller y agrupacion de rutas para acceder a controller de forma optimizada

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home(){
+        return view ('home');
+    }
+
+    public function blog(){
+          //Consulta a Base de datos
+    $posts = [
+        ['id' => 1, "title" => 'PHP', 'slug' => 'php'],
+        ['id' => 2, "title" => 'Laravel', 'slug' => 'laravel']
+    ];
+    return view('blog', ['posts' => $posts]);
+    }
+
+    public function post($slug){
+         //Consulta a Base de datos
+        $post = $slug;
+        return view('post', ['post' => $post]);
+    }
+}
