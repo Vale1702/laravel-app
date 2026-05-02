9802316f61578ae0327f155b34583c045fd9d731
Valentina <valentinahdz0296@gmail.com>
2026-05-02 00:06:18 -0600

    Se agrega logica para eliminar y se configuran las rutas para Index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,5 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource("posts", PostController::class)->except(["show"]);
 
 require __DIR__.'/auth.php';
