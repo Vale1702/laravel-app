50c94811e894ea544528d81b151d4eba2f092107
Valentina <valentinahdz0296@gmail.com>
2026-08-20 16:55:47 -0600

    integracion de update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,6 +30,15 @@ class PostController extends Controller
         return view("posts.edit", ["post" =>$post]);
     }
 
+    public function update(Request $request, Post $post ){
+        $post -> update([
+            'title' => $title = $request->title,
+            'slug' => Str::slug($title),
+            'body' =>$body = $request->body,
+        ]);
+        return redirect()->route('posts.edit', $post);
+    }
+
     public function destroy(Post $post){
         $post->delete();
         return back();
