f1262daad5a4b372eea28b5751bf24affedbf1c1
Valentina <valentinahdz0296@gmail.com>
2026-08-24 16:48:41 -0600

    Actualizacion de header

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,21 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto Web </title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <p>
-        <a href=" {{route('home') }} ">Home</a>
-        <a href=" {{route('blog') }} ">Blog</a>
-
-        <!-- Verifica si estamos logeados o no -->
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class=" flex items-center flex-grow gap-4">
+                <a href=" {{route('home') }} ">
+                    <img src="{{ asset('imagenes/logo.png') }}" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" placeholder="Buscar">
+                </form>
+            </div>
+            <!-- Verifica si estamos logeados o no -->
         @auth
         <a href="{{route('dashboard') }}">Dashboard</a>
         @else
         <a href="{{route('login') }}">login</a>
         @endauth
-    </p>
-    <hr>
-
-    @yield('content')
+        </header>
+        @yield('content')
+    </div>
 </body>
 </html>
