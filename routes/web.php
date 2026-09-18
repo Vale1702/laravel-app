f90d140cdc3bd4360786f381e5de21e87b29422c
Valentina <valentinahdz0296@gmail.com>
2026-09-17 23:18:29 -0600

    Creacion de componente home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(PageController::class)->group(function () {
-    Route::get("/", "home")->name("home");
+    Route::get("/", "home")->inertia("Home");
     Route::get("blog/{post:slug}", "post")->name("post");
 });
 
